86d575737cba0e99fd2c689c1f88f949c0b905fd
Jorge Rivera <latchdevel@users.noreply.github.com>
2021-03-28 13:02:22 +0200

    Set VERSION to 2 and add a license mention

--- pilight-usb-nano.ino
+++ pilight-usb-nano.ino
@@ -1,19 +1,13 @@
 /*
 	Copyright (C) 2015 CurlyMo
-
-	This file is part of pilight.
-
-	pilight is free software: you can redistribute it and/or modify it under the
-	terms of the GNU General Public License as published by the Free Software
-	Foundation, either version 3 of the License, or (at your option) any later
-	version.
-
-	pilight is distributed in the hope that it will be useful, but WITHOUT ANY
-	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-	A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+  This file is part of pilight. GNU General Public License v3.0.
 
 	You should have received a copy of the GNU General Public License
 	along with pilight. If not, see	<http://www.gnu.org/licenses/>
+
+  Copyright (C) 2021 Jorge Rivera. GNU General Public License v3.0.
+   - New version 2, fully Arduino IDE compatible
+
 */ 
 
 #include <stdlib.h>
@@ -32,7 +26,7 @@
 /* Number devided by 10 */
 #define MIN_PULSELENGTH 			8			//tested to work down to 30us pulsewidth (=2)
 #define MAX_PULSELENGTH 			1600
-#define VERSION								1
+#define VERSION								2
 
 volatile uint32_t minrawlen = 1000;
 volatile uint32_t maxrawlen = 0;
